b45d7d60884473761b3965ed64a8fc5288e61422
Maciej <maciej.zelazo@protonmail.com>
2019-07-30 03:01:17 +0200

    More content added

--- script.js
+++ script.js
@@ -43,9 +43,61 @@ if ($('.main-nav').length && $('.main-nav').attr('display') != 'none') {
     $('.mobile-nav-overly').hide();
 }
 
-$('.owl-carousel').owlCarousel({
+$('.owl-one').owlCarousel({
     loop: true,
     margin: 10,
     autoplay: true,
     items: 1
 })
+
+$('.owl-two').owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    items: 1
+})
+
+
+
+$(document).ready(function () {
+    var myDate = new Date();
+    myDate.setDate(myDate.getDate() + 10);
+    $("#countdown").countdown(myDate, function (event) {
+        $(this).html(
+            event.strftime(
+                '<div class="col-3"> <div class="time text-slate-dark">%D</div> <span class="text text-slate">Days</span> </div> <div class="col-3"> <div class="time text-slate-dark">%H</div><span class="text text-slate">Hours</span> </div> <div class="col-3"> <div class="time text-slate-dark">%M</div><span class="text text-slate">Minutes</span> </div> <div class="col-3"> <div class="time text-slate-dark">%S</div><span class="text text-slate">Seconds</span> </div>'
+            )
+        );
+    });
+
+});
+// Set the date we're counting down to
+// var countDownDate = new Date();
+// countDownDate.setDate(countDownDate.getDate() + 10);
+
+// // Update the count down every 1 second
+// var x = setInterval(function () {
+
+//     // Get today's date and time
+//     var now = new Date().getTime();
+
+//     // Find the distance between now and the count down date
+//     var distance = countDownDate - now;
+
+//     // Time calculations for days, hours, minutes and seconds
+//     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+//     $(".sale-timer__days").prepend(days);
+//     $(".sale-timer__hours").prepend(hours);
+//     $(".sale-timer__minutes").prepend(minutes);
+//     $(".sale-timer__seconds").prepend(seconds);
+
+//     // If the count down is over, write some text 
+//     if (distance < 0) {
+//         clearInterval(x);
+//         document.getElementById("countdownSale").innerHTML = "EXPIRED";
+//     }
+// }, 1000);
